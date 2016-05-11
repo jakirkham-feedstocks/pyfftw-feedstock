e2b3993e5edba5591469c4b04dffcb67b0e6913a
Gregory R. Lee <gregory.lee@cchmc.org>
2016-05-11 19:30:29 -0400

    build with statically linked FFTW to accidental dynamic linking to MKL FFT routines

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,8 @@
 
 export C_INCLUDE_PATH=$PREFIX/include  # required as fftw3.h installed here
 
+# define STATIC_FFTW_DIR so the patched setup.py will statically link FFTW
+export STATIC_FFTW_DIR=$PREFIX/lib
+
 $PYTHON setup.py build
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt --optimize=1
